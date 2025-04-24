877e5b8676ddd3b9302bd419d4fc6662abd994cf
ecc0503 <155265300+ecc0503@users.noreply.github.com>
2025-04-24 18:30:46 +0000

    feat: django rest api

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -1,23 +1,25 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import AppUserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
-from .models import AppUser, Team, Activity, Leaderboard, Workout
+from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
+from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = 'http://localhost:8000/'
+    # URLs para el codespace y localhost
+    codespace_url = 'https://psychic-happiness-q77gp49x5g6jc4x6j-8000.app.github.dev/'
+    localhost_url = 'http://localhost:8000/'
     return Response({
-        'users': base_url + 'api/users/?format=api',
-        'teams': base_url + 'api/teams/?format=api',
-        'activities': base_url + 'api/activities/?format=api',
-        'leaderboard': base_url + 'api/leaderboard/?format=api',
-        'workouts': base_url + 'api/workouts/?format=api'
+        'users': [codespace_url + 'api/users/?format=api', localhost_url + 'api/users/?format=api'],
+        'teams': [codespace_url + 'api/teams/?format=api', localhost_url + 'api/teams/?format=api'],
+        'activities': [codespace_url + 'api/activities/?format=api', localhost_url + 'api/activities/?format=api'],
+        'leaderboard': [codespace_url + 'api/leaderboard/?format=api', localhost_url + 'api/leaderboard/?format=api'],
+        'workouts': [codespace_url + 'api/workouts/?format=api', localhost_url + 'api/workouts/?format=api']
     })
 
-class AppUserViewSet(viewsets.ModelViewSet):
-    queryset = AppUser.objects.all()
-    serializer_class = AppUserSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
